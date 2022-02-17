89c441acbbe7d2af060607fdcf5b57ae2720bd53
krry <krryxq@163.com>
2022-02-17 20:50:38 +0800

    feat(love): code 优化

--- src/views/love.vue
+++ src/views/love.vue
@@ -2,34 +2,31 @@
 import { ref, Ref, computed } from 'vue'
 import { ElMessageBox, ElButton } from 'element-plus'
 
-const giftList: Ref<any> = ref({
-  买口红: { rate: 0.05, select: false, order: 0 }, // order：flex布局子元素顺序
-  买包包: { rate: 0.05, select: false, order: 1 },
-  再来一次: { rate: 0.3, select: false, order: 2 },
-  买衣服: { rate: 0.09, select: false, order: 5 },
-  btn: { text: '开始抽奖', order: 4 },
-  谢谢抽奖: { rate: 0.3, select: false, order: 8 },
-  抱一下: { rate: 0.3, select: false, order: 7 },
-  亲一口: { rate: 0.3, select: false, order: 6 },
-  有求必应: { rate: 0.01, select: false, order: 3 }
-})
+const giftList: Ref<any[]> = ref([
+  { name: '买口红', rate: 0.05, select: false, order: 0 }, // order：flex布局子元素顺序
+  { name: '买包包', rate: 0.05, select: false, order: 1 },
+  { name: '再来一次', rate: 0.3, select: false, order: 2 },
+  { name: '买衣服', rate: 0.09, select: false, order: 5 },
+  { name: '开始抽奖', order: 4 },
+  { name: '谢谢抽奖', rate: 0.3, select: false, order: 8 },
+  { name: '抱一下', rate: 0.3, select: false, order: 7 },
+  { name: '亲一口', rate: 0.3, select: false, order: 6 },
+  { name: '有求必应', rate: 0.01, select: false, order: 3 }
+])
 
-const giftNameList = computed(() => Object.keys(giftList.value))
 const selecting = ref(false)
 
 const rangeGiftList = computed(() => {
   const rangeList = []
   let min = 0
-  let index = 0 // index 是当前位置，选中时 index 的值
-  for (let key in giftList.value) {
-    index++
-    if (key === 'btn') continue
-    const max = min + giftList.value[key].rate
+  for (const [index, val] of giftList.value.entries()) {
+    if (index === 4) continue
+    const max = min + val.rate
     rangeList.push({
       min,
       max,
-      index, // 从 1 开始
-      gift: key
+      index: index + 1, // index 是当前位置，选中时 index 的值。从 1 开始
+      gift: val.name
     })
     min = max
   }
@@ -55,13 +52,13 @@ const start = () => {
   }
   startChange(0, selectIndex, 5, 1, () => {
     if (isMobile()) {
-      alert(`哇哦！你抽到了${gift}`)
+      alert(`哇哦！你抽到了【${gift}】`)
     } else {
-      ElMessageBox.alert(`哇哦！你抽到了${gift}`, '奖品公示', {
+      ElMessageBox.alert(`哇哦！你抽到了【${gift}】`, '奖品公示', {
         confirmButtonText: 'OK'
       })
     }
-    giftList.value.btn.text = '再抽一次'
+    giftList.value[4].name = '再抽一次'
     selecting.value = false
   })
 }
@@ -81,15 +78,13 @@ const startChange = (
   } else {
     min === 4 ? (min += 2) : min++
   }
-  let index = 0
-  for (let key in giftList.value) {
+  for (const [index, val] of giftList.value.entries()) {
     if (index !== 4) {
-      giftList.value[key].select = false
+      val.select = false
       if (index === min - 1) {
-        giftList.value[key].select = true
+        val.select = true
       }
     }
-    index++
   }
   if (min < max || count !== 1) {
     time += 0.6
@@ -121,18 +116,18 @@ const isMobile = () => {
   <p>祝老婆情人节快乐！！快来选礼物吧~</p>
   <div class="gift-container">
     <div
-      :class="['gift-container__single', { selected: giftList[ele].select }]"
-      :style="{ order: giftList[ele].order }"
-      v-for="ele in giftNameList"
-      :key="ele"
+      :class="['gift-container__single', { selected: ele.select }]"
+      :style="{ order: ele.order }"
+      v-for="(ele, index) in giftList"
+      :key="index"
     >
-      <div class="btn" v-if="ele === 'btn'">
+      <div class="btn" v-if="index === 4">
         <el-button v-if="!selecting" @click="start">
-          {{ giftList[ele].text }}
+          {{ ele.name }}
         </el-button>
         <span v-else>抽奖中...</span>
       </div>
-      <div class="content">{{ ele }}</div>
+      <div class="content">{{ ele.name }}</div>
     </div>
   </div>
 </template>
