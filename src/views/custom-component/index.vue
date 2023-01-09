26257706a74369f92f9c35d3fdcaa0e3484ac9b8
krryxa <krryxq@163.com>
2023-01-09 14:34:03 +0800

    feat(product): 自定义组件2

--- src/views/custom-component/index.vue
+++ src/views/custom-component/index.vue
@@ -1,25 +1,37 @@
 <script setup lang="ts">
 import { ref } from 'vue'
 import subCompFirst from './sub-comp1.vue'
+import subCompSecond from './sub-comp2.vue'
 
-const value1 = ref(['啦啦啦', '1212'])
+const value = ref(['啦啦啦', '1212'])
+const leftVal = ref('show fast')
+const rightVal = ref('only one')
 </script>
 
 <template>
   <h1>自定义组件</h1>
-  <div class="first">
-    <div class="show">绑定值：{{ value1 }}</div>
-    <sub-comp-first name="自定义组件1" v-model="value1"></sub-comp-first>
+  <div class="wrap">
+    <sub-comp-first name="自定义组件1" v-model="value" />
+    <div class="show">绑定值：{{ value }}</div>
+  </div>
+  <div class="wrap">
+    <sub-comp-second
+      name="自定义组件2"
+      v-model:first="leftVal"
+      v-model:second="rightVal"
+    />
+    <div class="show">first 绑定值：{{ leftVal }}</div>
+    <div class="show">second 绑定值：{{ rightVal }}</div>
   </div>
 </template>
 
 <style lang="scss" scoped>
-.first {
+.wrap {
   display: flex;
   flex-direction: column;
-}
-
-.show {
-  margin-bottom: 10px;
+  margin-bottom: 30px;
+  .show {
+    margin-top: 10px;
+  }
 }
 </style>
---
+++ src/views/custom-component/sub-comp2.vue
@@ -0,0 +1,56 @@
+<script setup lang="ts">
+import { computed } from 'vue'
+
+interface Props {
+  first: string // 具名绑定 v-model:first
+  second: string
+  name: string
+}
+
+// 采用基于类型声明 props，用 withDefaults 指定默认值
+const props = withDefaults(defineProps<Props>(), {
+  first: '',
+  second: '',
+  name: '组件'
+})
+const emit = defineEmits(['update:first', 'update:second'])
+
+// 可写计算属性，当改变 firstVal，就会触发 set 方法
+const firstVal = computed({
+  get: () => props.first,
+  set: (val) => emit('update:first', val)
+})
+const secondVal = computed({
+  get: () => props.second,
+  set: (val) => emit('update:second', val)
+})
+</script>
+
+<template>
+  <div class="container">
+    <span class="label">{{ name }}</span>
+    <el-input v-model="firstVal" placeholder="Please input" />
+    <el-input v-model="secondVal" placeholder="Please input" />
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.container {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  .label {
+    margin-right: 10px;
+  }
+
+  .el-input {
+    width: 200px;
+    margin-right: 10px;
+
+    &:last-child {
+      margin-right: 0;
+    }
+  }
+}
+</style>
